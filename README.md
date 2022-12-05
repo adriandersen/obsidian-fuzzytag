22c953c29bc6cbf94e2b4c971502469242c3ce1f
Adrian Andersen <adriandersen3@gmail.com>
2022-12-05 16:45:51 +0100

    initial working version

--- README.md
+++ README.md
@@ -1,72 +1,64 @@
-# Obsidian Sample Plugin
+# Fuzzytag
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+Based on the Obsidian Frontmatter Tag Suggest plugin
+https://github.com/jmilldotdev/obsidian-frontmatter-tag-suggest/commit/d80bcfb64d96d7fcb908deb5f4b0c9c8041c267c
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+This plugin has been made because fuzzy search would change the functionality of the original plugin, which might not be desired by everyone.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+## Known issues:
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+Hacky implementation of highlighting in suggestions means that tags with special characters (`<` or `>`) might break. Feel free to submit a PR if this doesnt work with your workflow.
 
-## First time developing plugins?
+---
 
-Quick starting guide for new plugin devs:
-
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+-   Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
+-   Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
+-   Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
+-   Install NodeJS, then run `npm i` in the command line under your repo folder.
+-   Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
+-   Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
+-   Reload Obsidian to load the new version of your plugin.
+-   Enable plugin in settings window.
+-   For updates to the Obsidian API run `npm update` in the command line under your repo folder.
 
 ## Releasing new releases
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+-   Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
+-   Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
+-   Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
+-   Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
+-   Publish the release.
 
 > You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
 > The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
 
 ## Adding your plugin to the community plugin list
 
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+-   Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
+-   Publish an initial version.
+-   Make sure you have a `README.md` file in the root of your repo.
+-   Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
 
 ## How to use
 
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
+-   Clone this repo.
+-   `npm i` or `yarn` to install dependencies
+-   `npm run dev` to start compilation in watch mode.
 
 ## Manually installing the plugin
 
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
+-   Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
 
 ## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
 
+-   [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code.
+-   To use eslint with this project, make sure to install eslint from terminal:
+    -   `npm install -g eslint`
+-   To use eslint to analyze this project use this command:
+    -   `eslint main.ts`
+    -   eslint will then create a report with suggestions for code improvement by file and line number.
+-   If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
+    -   `eslint .\src\`
 
 ## API Documentation
 
--- main.ts
+++ main.ts
@@ -1,89 +1,38 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	EditorPosition,
+	EditorSuggest,
+	EditorSuggestContext,
+	EditorSuggestTriggerInfo,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
+const fuzzysort = require("fuzzysort");
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginSettings {
+	matchColor: string;
 }
+const DEFAULT_SETTINGS: PluginSettings = {
+	matchColor: "#ff0000",
+};
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class FrontmatterFuzzyTagPlugin extends Plugin {
+	settings: PluginSettings;
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEditorSuggest(new FuzzyTag(this));
+		this.addSettingTab(new FuzzyTagSettingsTab(this.app, this));
 	}
-
-	onunload() {
-
-	}
-
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,47 +40,136 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class FuzzyTag extends EditorSuggest<string> {
+	plugin: FrontmatterFuzzyTagPlugin;
+	tags: (string | undefined)[];
+
+	constructor(plugin: FrontmatterFuzzyTagPlugin) {
+		super(plugin.app);
+		this.plugin = plugin;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getAllTagsWithoutHashtag(): (string | undefined)[] {
+		//@ts-expect-error, private method
+		const tags: any = this.plugin.app.metadataCache.getTags();
+		return [...Object.keys(tags)].map((p) => p.split("#").pop());
+	}
+	inRange(range: string) {
+		if (!range || !range.length) return false;
+		if (range.match(/^---\n/gm)?.length != 1) return false;
+		if (!/^tags?:/gm.test(range)) return false;
+		const split = range.split(/(^\w+:?\s*\n?)/gm);
+		for (let i = split.length - 1; i >= 0; i--) {
+			if (/(^\w+:?\s*\n?)/gm.test(split[i]))
+				return split[i].startsWith("tags:");
+		}
+		return false;
+	}
+	inline = false;
+	onTrigger(
+		cursor: EditorPosition,
+		editor: Editor,
+		_: TFile
+	): EditorSuggestTriggerInfo | null {
+		const lineContents = editor.getLine(cursor.line).toLowerCase();
+		const onFrontmatterTagLine =
+			lineContents.startsWith("tags:") ||
+			lineContents.startsWith("tag:") ||
+			this.inRange(editor.getRange({ line: 0, ch: 0 }, cursor));
+		if (onFrontmatterTagLine) {
+			this.inline =
+				lineContents.startsWith("tags:") ||
+				lineContents.startsWith("tag:");
+			const sub = editor.getLine(cursor.line).substring(0, cursor.ch);
+			const match = sub.match(/(\S+)$/)?.first();
+			if (match) {
+				this.tags = this.getAllTagsWithoutHashtag();
+				const matchData = {
+					end: cursor,
+					start: {
+						ch: sub.lastIndexOf(match),
+						line: cursor.line,
+					},
+					query: match,
+				};
+				return matchData;
+			}
+		}
+		return null;
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	getSuggestions(context: EditorSuggestContext): string[] {
+		// const suggestions = this.tags.filter((p) =>
+		// 	p.toLowerCase().contains(context.query.toLowerCase())
+		// );
+		const suggestions = fuzzysort.go(
+			context.query.toLowerCase(),
+			this.tags,
+			{
+				all: true,
+			}
+		);
+
+		return suggestions.map((p: any) =>
+			fuzzysort.highlight(
+				p,
+				`<span style="color: ${this.plugin.settings.matchColor}"><b>`,
+				"</b></span>"
+			)
+		);
+	}
+
+	renderSuggestion(suggestion: string, el: HTMLElement): void {
+		const outer = el.createDiv({ cls: "ES-suggester-container" });
+		outer.createDiv({ cls: "ES-tags" }).innerHTML = `#${suggestion}`;
+	}
+
+	selectSuggestion(suggestion: string): void {
+		if (this.context) {
+			if (this.inline) {
+				suggestion = `"${suggestion}",`;
+			} else {
+				suggestion = `${suggestion}\n -`;
+			}
+			(this.context.editor as Editor).replaceRange(
+				//This might break if you use special characters in your tags
+				`${suggestion.replace(/<\/?[^>]+(>|$)/g, "")} `,
+				this.context.start,
+				this.context.end
+			);
+		}
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class FuzzyTagSettingsTab extends PluginSettingTab {
+	plugin: FrontmatterFuzzyTagPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: FrontmatterFuzzyTagPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "Fuzzy tag autocomplete settings" });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Fuzzy match color")
+			.setDesc(
+				"What color to display on matched letters in the fuzzy match"
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("#ff9900")
+					.setValue(this.plugin.settings.matchColor)
+					.onChange(async (value) => {
+						console.log("Secret: " + value);
+						this.plugin.settings.matchColor = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
